15a42604c9d40f56d008c497dc081c31fe495fd9
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2021-03-22 20:21:05 -0400

    waittime.py demo

---
+++ WaitTime/waittime.py
@@ -0,0 +1,34 @@
+from locust import User, task, constant, between, constant_pacing, SequentialTaskSet
+import time
+
+
+class ConstantPacing(User):
+    wait_time = constant_pacing(3)
+
+    @task
+    def launch(self):
+        time.sleep(2)
+        print("Constant Pacing Demo")
+
+
+class Constant(User):
+    wait_time = constant(3)
+
+    @task
+    def launch(self):
+        print("Constant Demo")
+
+
+class Between(User):
+    wait_time = between(3, 5)
+
+    @task
+    def launch(self):
+        print("Between Demo")
+
+
+class Demo(SequentialTaskSet):
+
+    tasks = [Between, Constant, ConstantPacing]
+
+
