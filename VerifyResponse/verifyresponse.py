4b8a3d60fd76f5bcf1a88ba07ff790c02c31d289
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2021-03-26 22:02:40 -0400

    Validating the response

--- VerifyResponse/verifyresponse.py
+++ VerifyResponse/verifyresponse.py
@@ -5,8 +5,18 @@ class MyScript(SequentialTaskSet):
 
     @task
     def get_xml(self):
-        res = self.client.get("/xml", name="XML")
-        print(res)
+        result = self.client.get("/xml", name="XML")
+        print(result)
+
+    @task
+    def get_json(self):
+        expected_response = "Wake up to WonderWidgets!"
+
+        with self.client.get("/json", catch_response=True, name="JSON") as response:
+            result = True if expected_response in response.text else False  # Pythonic if else statement
+            print(self.get_json.__name__, result)  # Prints True if the response contains `expected_response`,
+            # else False
+            response.success()  # Marking this as success
 
     @task
     def get_robots(self):
@@ -15,18 +25,8 @@ class MyScript(SequentialTaskSet):
         with self.client.get("/robots.txt", catch_response=True, name="Robots") as response:
             if expected_response in response.text:
                 result = "Success"
-                response.failure("Not a failure")  # Marking this success request as intentional failure
+                response.failure("Not a failure")  # Marking this success request intentional failure
         print(self.get_robots.__name__, result)
-
-    @task
-    def get_json(self):
-        expected_response = "Wake up to WonderWidgets!"
-
-        with self.client.get("/json", catch_response=True, name="JSON") as response:
-            result = True if expected_response in response.text else False  # Pythonic way
-            print(self.get_json.__name__, result)  # Prints True if the response contains `expected_response`,
-            # else False
-            response.success()  # Marking this as success
 
     @task
     def get_failure(self):
